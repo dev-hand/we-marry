6d7fa542c2bfa8d42101e428f4c0c304971ef5ab
dev-hand <wjdgns200297@gmail.com>
2023-09-15 18:21:22 +0900

    deploy to gh-pages

--- _next/static/MPMaS2yZ6Pz8GJsfTyosb/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-1230e6dc6bc17d1c.js"],"/_error":["static/chunks/pages/_error-4b23c55d6d5b5ed5.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
